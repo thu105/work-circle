5503e9e9d3f105187b45970ce9ef9f55f861ed2c
Harshil Shah <harshilshah@Harshils-MacBook-Pro.local>
2023-03-16 23:30:44 -0700

    Updating members page to get the username

--- src/members.php
+++ src/members.php
@@ -11,6 +11,21 @@ if(!$loggedin){
 
 // Add Member Directory Here
 
+if (isset($_GET['view'])) {
+  $view = sanitizeString($_GET['view']);
+
+  if ($view == $user) $name = "Your";
+    else                $name = "$view's";
+
+  // echo "<h3>$name Profile</h3>";
+  // showProfile($view);
+
+  // echo "<a data-role='button' data-transition='slide'
+  //         href='messages.php?view=$view&r=$randstr'>View $name messages</a>";
+  // die("</div></body></html>");
+
+  
+}
 ?>
 
 
